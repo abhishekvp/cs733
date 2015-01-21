90c186ade3c702f989a1dd7757f798a8099ee1a3
Abhishek Potnis <abhishekp.bugzilla@gmail.com>
2015-01-22 01:52:38 +0530

    Used go fmt tool to format the files

--- assignment1/server.go
+++ assignment1/server.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net"
 	"os"
-	"math/rand"
-	"strings"
 	"strconv"
+	"strings"
 	"time"
-
 )
 
 const port string = ":9000"
@@ -19,17 +18,16 @@ const port string = ":9000"
 * 	kvMap[key]["value"]
 * 	kvMap[key]["numbytes"]
 * 	kvMap[key]["version"]
-*/
+ */
 var kvMap map[string]map[string]string
 
-
 func main() {
-	
+
 	kvMap = make(map[string]map[string]string)
 
 	/**
 	TCP Connection Code referred from "Chapter 3. Socket-level Programming, Network programming with Go"
-	URL: http://jan.newmarch.name/go/socket/chapter-socket.html 
+	URL: http://jan.newmarch.name/go/socket/chapter-socket.html
 	**/
 	tcpAddr, err := net.ResolveTCPAddr("tcp", port)
 	checkError(err)
@@ -49,7 +47,6 @@ func main() {
 	}
 }
 
-
 func processClient(conn net.Conn) {
 
 	var bufLine1 [512]byte
@@ -57,7 +54,6 @@ func processClient(conn net.Conn) {
 	var cLine1 []string
 	var cLine2 string
 	var err2 error
-
 
 	for {
 		// Read First Line from the client
@@ -69,9 +65,8 @@ func processClient(conn net.Conn) {
 
 		cLine1 = strings.Fields(string(bufLine1[0:]))
 
-	
 		//Read second line, containing the value entered at the client
-		if cLine1[0]=="set" || cLine1[0] == "cas"{
+		if cLine1[0] == "set" || cLine1[0] == "cas" {
 			_, err = conn.Read(bufLine2[0:])
 			if err != nil {
 				checkError(err)
@@ -80,10 +75,10 @@ func processClient(conn net.Conn) {
 			//Clean the line read, off trailing carriage return and newline
 			cLine2 = strings.TrimSpace(string(bufLine2[0:]))
 			if strings.Contains(cLine2, "\r") {
-				cLine2 = strings.Trim(cLine2,"\r")
+				cLine2 = strings.Trim(cLine2, "\r")
 			}
-			if strings.Contains(cLine2,"\n") {
-				cLine2 = strings.Trim(cLine2,"\n")
+			if strings.Contains(cLine2, "\n") {
+				cLine2 = strings.Trim(cLine2, "\n")
 			}
 
 		}
@@ -91,93 +86,88 @@ func processClient(conn net.Conn) {
 		//Select action based on the first word of the query entered at the client
 		switch cLine1[0] {
 
-				case "set":
-							kvMap[cLine1[1]] = map[string]string{"exptime" : cLine1[2], "numbytes" : cLine1[3], "value" : cLine2 , "version" : strconv.FormatInt(rand.Int63(),10)}
-							//The Key-Value has pair has 0 expiry time, so it should never expire.
-							if(kvMap[cLine1[1]]["exptime"]!="0") {
-								go processExpTime(cLine1[1]);
-							}
-							//Check for [noreply]
-							//if strings.Contains(cLine1[4],"[noreply]") != true {
-							_, err2 = conn.Write([]byte("OK " + kvMap[cLine1[1]]["version"]+"\n"))
-							//}
+		case "set":
+			kvMap[cLine1[1]] = map[string]string{"exptime": cLine1[2], "numbytes": cLine1[3], "value": cLine2, "version": strconv.FormatInt(rand.Int63(), 10)}
+			//The Key-Value has pair has 0 expiry time, so it should never expire.
+			if kvMap[cLine1[1]]["exptime"] != "0" {
+				go processExpTime(cLine1[1])
+			}
+			//Check for [noreply]
+			//if strings.Contains(cLine1[4],"[noreply]") != true {
+			_, err2 = conn.Write([]byte("OK " + kvMap[cLine1[1]]["version"] + "\n"))
+			//}
 
-						
-				case "get":
-							_, err2 = conn.Write([]byte("VALUE "))
-							_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1],"\r\n")]["numbytes"]+"\n"))
-							_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1],"\r\n")]["value"]))
+		case "get":
+			_, err2 = conn.Write([]byte("VALUE "))
+			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["numbytes"] + "\n"))
+			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["value"]))
 
-				case "getm":
-							_, err2 = conn.Write([]byte("VALUE "))
-							_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1],"\r\n")]["version"]+"\t"))
-							_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1],"\r\n")]["exptime"]+"\t"))
-							_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1],"\r\n")]["numbytes"]+"\n"))
-							_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1],"\r\n")]["value"]))							
-				
-				case "cas":
-							if cLine1[3] == kvMap[cLine1[1]]["version"] {
+		case "getm":
+			_, err2 = conn.Write([]byte("VALUE "))
+			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["version"] + "\t"))
+			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["exptime"] + "\t"))
+			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["numbytes"] + "\n"))
+			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["value"]))
 
-								kvMap[cLine1[1]]["value"] = cLine2
-								kvMap[cLine1[1]]["exptime"] = cLine1[2]
-								kvMap[cLine1[1]]["numbytes"] = cLine1[4]
-								kvMap[cLine1[1]]["version"] = strconv.FormatInt(rand.Int63(),10)
-								//The Key-Value has pair has 0 expiry time, so it should never expire.
-								if(kvMap[cLine1[1]]["exptime"]!="0") {
-									go processExpTime(cLine1[1]);
-								}
-								//Check for [noreply]
-								//if strings.Contains(cLine1[5],"[noreply]") != true {
-									_, err2 = conn.Write([]byte("OK " + kvMap[cLine1[1]]["version"]+"\n"))
-								//}						
-							}
+		case "cas":
+			if cLine1[3] == kvMap[cLine1[1]]["version"] {
 
-				case "delete":
-							delete(kvMap, cLine1[1])
-							_, err2 = conn.Write([]byte("DELETED\n"))
+				kvMap[cLine1[1]]["value"] = cLine2
+				kvMap[cLine1[1]]["exptime"] = cLine1[2]
+				kvMap[cLine1[1]]["numbytes"] = cLine1[4]
+				kvMap[cLine1[1]]["version"] = strconv.FormatInt(rand.Int63(), 10)
+				//The Key-Value has pair has 0 expiry time, so it should never expire.
+				if kvMap[cLine1[1]]["exptime"] != "0" {
+					go processExpTime(cLine1[1])
+				}
+				//Check for [noreply]
+				//if strings.Contains(cLine1[5],"[noreply]") != true {
+				_, err2 = conn.Write([]byte("OK " + kvMap[cLine1[1]]["version"] + "\n"))
+				//}
+			}
 
+		case "delete":
+			delete(kvMap, cLine1[1])
+			_, err2 = conn.Write([]byte("DELETED\n"))
 
 		}
 
 		if err2 != nil {
 			return
 		}
-		
-					
-		}
 
 	}
+
+}
 
 /**
 * @param key String
 * Updates the "exptime" field of the key, by decrementing at the interval of one second.
 * Once the exptime is up, the key-value pair is deleted from the Map
-*/
+ */
 func processExpTime(key string) {
 
-    ticker := time.NewTicker(time.Millisecond * 1000)
-    var t time.Time
-    exptime, err := strconv.Atoi(kvMap[key]["exptime"])
-   
-    if err != nil {
-    	fmt.Println("Error")
-    	return
-    }
+	ticker := time.NewTicker(time.Millisecond * 1000)
+	var t time.Time
+	exptime, err := strconv.Atoi(kvMap[key]["exptime"])
 
-    go func() {
-        for t = range ticker.C {
-            exptime = exptime - 1;
-            kvMap[key]["exptime"] = strconv.Itoa(exptime)
-           
-        }
-    }()
-    time.Sleep(time.Duration(exptime)*time.Second)
-    ticker.Stop()
-    delete(kvMap, key)
-    
+	if err != nil {
+		fmt.Println("Error")
+		return
+	}
+
+	go func() {
+		for t = range ticker.C {
+			exptime = exptime - 1
+			kvMap[key]["exptime"] = strconv.Itoa(exptime)
+
+		}
+	}()
+	time.Sleep(time.Duration(exptime) * time.Second)
+	ticker.Stop()
+	delete(kvMap, key)
 
 }
-
 
 //Error handler
 func checkError(err error) {
--- assignment1/server_test.go
+++ assignment1/server_test.go
@@ -1,48 +1,49 @@
 package main
+
 import (
-"net"
-"os"
-"testing"
-"strings"
-"time"
+	"net"
+	"os"
+	"strings"
+	"testing"
+	"time"
 )
- 
+
 const servAddr string = "localhost:9000"
 
 func Test_Set(t *testing.T) {
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
 	if err != nil {
-	os.Exit(1)
+		os.Exit(1)
 	}
 
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
-	os.Exit(1)
+		os.Exit(1)
 	}
 
 	_, err = conn.Write([]byte("set foo 0 1"))
 	if err != nil {
-	os.Exit(1)
+		os.Exit(1)
 	}
 
-	time.Sleep(time.Duration(1)*time.Second)
+	time.Sleep(time.Duration(1) * time.Second)
 
 	_, err = conn.Write([]byte("bar"))
 	if err != nil {
-	os.Exit(1)
+		os.Exit(1)
 	}
 
 	reply := make([]byte, 1024)
-	 
+
 	_, err = conn.Read(reply)
 	if err != nil {
-	os.Exit(1)
+		os.Exit(1)
 	}
 
 	if strings.Fields(string(reply))[0] != "OK" {
 		t.Error("Setting did not work as expected.")
-		
+
 	}
 	conn.Close()
 
