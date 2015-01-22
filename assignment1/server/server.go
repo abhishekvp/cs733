dd206687277ff4bc61dc4804cade57f14928bed8
Abhishek Potnis <abhishekp.bugzilla@gmail.com>
2015-01-22 19:54:38 +0530

    Added support for error reporting as per protocol spec

--- assignment1/server/server.go
+++ assignment1/server/server.go
@@ -11,6 +11,7 @@ import (
 
 const port string = ":9000"
 
+
 /*
 * Map of Maps to store data.
 * 	kvMap[key]["exptime"]
@@ -28,49 +29,50 @@ func main() {
 	TCP Connection Code referred from "Chapter 3. Socket-level Programming, Network programming with Go"
 	URL: http://jan.newmarch.name/go/socket/chapter-socket.html
 	**/
-	tcpAddr, err := net.ResolveTCPAddr("tcp", port)
-	checkError(err)
+	tcpAddr, connError := net.ResolveTCPAddr("tcp", port)
 
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	checkError(err)
+	listener, connError := net.ListenTCP("tcp", tcpAddr)
 
 	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			continue
-		}
+		conn, genError := listener.Accept()
+		checkError(connError, conn)
+		checkError(genError, conn)
 
 		// Goroutine to handle multiple clients
 		go processClient(conn)
 
 	}
+	
 }
 
+
 func processClient(conn net.Conn) {
+
+
+
+	for {
 
 	var bufLine1 [512]byte
 	var bufLine2 [512]byte
 	var cLine1 []string
 	var cLine2 string
-	var err2 error
 
-	for {
+
 		// Read First Line from the client
-		_, err := conn.Read(bufLine1[0:])
-		if err != nil {
-			checkError(err)
-			return
-		}
-
+		_, genError := conn.Read(bufLine1[0:])
+		checkError(genError, conn)
+		
+		//Count the number of spaces in the line received from the client
+		count:=strings.Count(string(bufLine1[0:])," ")		
+		fmt.Println("Count = "+ strconv.Itoa(count))
+		
 		cLine1 = strings.Fields(string(bufLine1[0:]))
 
 		//Read second line, containing the value entered at the client
 		if cLine1[0] == "set" || cLine1[0] == "cas" {
-			_, err = conn.Read(bufLine2[0:])
-			if err != nil {
-				checkError(err)
-				return
-			}
+			_, genError = conn.Read(bufLine2[0:])
+			checkError(genError, conn)
+
 			//Clean the line read, off trailing carriage return and newline
 			cLine2 = strings.TrimSpace(string(bufLine2[0:]))
 			if strings.Contains(cLine2, "\r") {
@@ -86,58 +88,121 @@ func processClient(conn net.Conn) {
 		switch cLine1[0] {
 
 		case "set":
-			kvMap[cLine1[1]] = map[string]string{"exptime": cLine1[2], "numbytes": cLine1[3], "value": cLine2, "version": strconv.FormatInt(rand.Int63(), 10)}
-			//The Key-Value has pair has 0 expiry time, so it should never expire.
-			if kvMap[cLine1[1]]["exptime"] != "0" {
-				go processExpTime(cLine1[1])
-			}
-			//Check for [noreply]
-			//if strings.Contains(cLine1[4],"[noreply]") != true {
-			_, err2 = conn.Write([]byte("OK " + kvMap[cLine1[1]]["version"] + "\n"))
-			//}
-
-		case "get":
-			_, err2 = conn.Write([]byte("VALUE "))
-			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["numbytes"] + "\n"))
-			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["value"]))
-
-		case "getm":
-			_, err2 = conn.Write([]byte("VALUE "))
-			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["version"] + "\t"))
-			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["exptime"] + "\t"))
-			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["numbytes"] + "\n"))
-			_, err2 = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["value"]))
-
-		case "cas":
-			if cLine1[3] == kvMap[cLine1[1]]["version"] {
-
-				kvMap[cLine1[1]]["value"] = cLine2
-				kvMap[cLine1[1]]["exptime"] = cLine1[2]
-				kvMap[cLine1[1]]["numbytes"] = cLine1[4]
-				kvMap[cLine1[1]]["version"] = strconv.FormatInt(rand.Int63(), 10)
+			//The "Space" count for a legal set query should be minimum 3
+			if count >= 3 {
+				kvMap[cLine1[1]] = map[string]string{"exptime": cLine1[2], "numbytes": cLine1[3], "value": cLine2, "version": strconv.FormatInt(rand.Int63(), 10)}
 				//The Key-Value has pair has 0 expiry time, so it should never expire.
 				if kvMap[cLine1[1]]["exptime"] != "0" {
 					go processExpTime(cLine1[1])
 				}
-				//Check for [noreply]
-				//if strings.Contains(cLine1[5],"[noreply]") != true {
-				_, err2 = conn.Write([]byte("OK " + kvMap[cLine1[1]]["version"] + "\n"))
-				//}
+				//Handle the "[noreply]" case
+				if count == 4 {
+					//Invalid argument instead of "[noreply]"
+					if strings.Contains(cLine1[4],"[noreply]") != true {
+						//CommandLine Formatting Error
+						ERRCMDERR(conn)
+					}
+				} else {
+					_, genError = conn.Write([]byte("OK " + kvMap[cLine1[1]]["version"] + "\n"))
+				}
+			} else {
+				//CommandLine Formatting Error
+				ERRCMDERR(conn)
 			}
 
+		case "get":
+			//The "Space" count for a legal get query should be 1
+			if count == 1 {
+				if _, ok := kvMap[strings.Trim(cLine1[1], "\r\n")]["value"]; ok {
+					_, genError = conn.Write([]byte("VALUE "))
+					_, genError = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["numbytes"] + "\n"))
+					_, genError = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["value"]))
+				} else {
+					//Key Not Found Error
+					ERRNOTFOUND(conn)
+				}
+			} else {
+				//CommandLine Formatting Error
+				ERRCMDERR(conn)	
+			}
+
+		case "getm":
+			//The "Space" count for a legal get query should be 1
+			if count == 1 {
+				if _, ok := kvMap[strings.Trim(cLine1[1], "\r\n")]["value"]; ok {
+					_, genError = conn.Write([]byte("VALUE "))
+					_, genError = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["version"] + "\t"))
+					_, genError = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["exptime"] + "\t"))
+					_, genError = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["numbytes"] + "\n"))
+					_, genError = conn.Write([]byte(kvMap[strings.Trim(cLine1[1], "\r\n")]["value"]))
+				} else {
+					//Key Not Found Error
+					ERRNOTFOUND(conn)
+				}
+			} else {
+				//CommandLine Formatting Error
+				ERRCMDERR(conn)	
+			}
+
+		case "cas":
+			//The "Space" count for a legal cas query should be 4
+			if count >= 4 {
+
+				if cLine1[3] == kvMap[cLine1[1]]["version"] {
+
+					kvMap[cLine1[1]]["value"] = cLine2
+					kvMap[cLine1[1]]["exptime"] = cLine1[2]
+					kvMap[cLine1[1]]["numbytes"] = cLine1[4]
+					kvMap[cLine1[1]]["version"] = strconv.FormatInt(rand.Int63(), 10)
+					//The Key-Value has pair has 0 expiry time, so it should never expire.
+					if kvMap[cLine1[1]]["exptime"] != "0" {
+						go processExpTime(cLine1[1])
+					}
+					//Handle the "[noreply]" case
+					if count == 5 {
+						//Invalid argument instead of "[noreply]"
+						if strings.Contains(cLine1[5],"[noreply]") != true {
+							//CommandLine Formatting Error
+							ERRCMDERR(conn)
+						}
+					} else {
+						_, genError = conn.Write([]byte("OK " + kvMap[cLine1[1]]["version"] + "\n"))
+						checkError(genError, conn)
+					}
+				} else {
+				//Version Mismatch. Value not updated.
+				ERR_VERSION(conn)
+				}
+			} else {
+				//CommandLine Formatting Error
+				ERRCMDERR(conn)
+			}
+
+
 		case "delete":
-			delete(kvMap, cLine1[1])
-			_, err2 = conn.Write([]byte("DELETED\n"))
+			//The "Space" count for a legal delete query should be 1
+			if count == 1 {
+				if _, ok := kvMap[strings.Trim(cLine1[1], "\r\n")]["value"]; ok {
+					delete(kvMap, cLine1[1])
+					_, genError := conn.Write([]byte("DELETED"+"\n"))
+					checkError(genError, conn)
 
+				} else {
+					//Key Not Found Error
+					ERRNOTFOUND(conn)
+				}
+			} else {
+				//CommandLine Formatting Error
+				ERRCMDERR(conn)	
+			}
 		}
-
-		if err2 != nil {
-			return
-		}
-
 	}
 
+	
 }
+
+	
+
 
 /**
 * @param key String
@@ -146,31 +211,51 @@ func processClient(conn net.Conn) {
  */
 func processExpTime(key string) {
 
-	ticker := time.NewTicker(time.Millisecond * 1000)
-	var t time.Time
-	exptime, err := strconv.Atoi(kvMap[key]["exptime"])
 
-	if err != nil {
-		fmt.Println("Error")
-		return
-	}
 
-	go func() {
-		for t = range ticker.C {
-			exptime = exptime - 1
-			kvMap[key]["exptime"] = strconv.Itoa(exptime)
+		ticker := time.NewTicker(time.Millisecond * 1000)
+		var t time.Time
+		exptime, _ := strconv.Atoi(kvMap[key]["exptime"])
+		go func() {
+			for t = range ticker.C {
+				exptime = exptime - 1
+				// Check for case when the key value pair is deleted or not found
+				if _, ok := kvMap[key]["exptime"]; ok {
+					kvMap[key]["exptime"] = strconv.Itoa(exptime)
+				}
 
-		}
-	}()
-	time.Sleep(time.Duration(exptime) * time.Second)
-	ticker.Stop()
-	delete(kvMap, key)
+			}
+		}()
+		time.Sleep(time.Duration(exptime) * time.Second)
+		ticker.Stop()
+		delete(kvMap, key)
+	
 
 }
 
 //Error handler
-func checkError(err error) {
-	if err != nil {
-		//fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+func checkError(genError error, conn net.Conn) {
+	if genError != nil {
+		err := "ERR_INTERNAL\n"
+		_, genError = conn.Write([]byte(err))
 	}
+}
+
+func ERRCMDERR(conn net.Conn) {
+	var genError error
+	err := "ERRCMDERR\n"
+	_, genError = conn.Write([]byte(err))
+}
+
+func ERRNOTFOUND(conn net.Conn) {
+	var genError error
+	err := "ERRNOTFOUND\n"
+	_, genError = conn.Write([]byte(err))
+}
+
+
+func ERR_VERSION(conn net.Conn) {
+	var genError error
+	err := "ERR_VERSION\n"
+_	, genError = conn.Write([]byte(err))
 }
