680745613ab44766bf6e4b71a3018ef99720b784
Abhishek Potnis <abhishekp.bugzilla@gmail.com>
2015-01-26 01:22:44 +0530

    Used channel to read from connection

--- assignment1/server/server_test.go
+++ assignment1/server/server_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"regexp"
 	"strings"
 	"testing"
 	"time"
@@ -11,75 +12,77 @@ import (
 
 const servAddr string = "localhost:9000"
 
-func Test_Set(t *testing.T) {
+var conn net.Conn
 
+func Test_Cases(t *testing.T) {
+	
 	go main()
 
 	//Connect to the Server
+
 	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
 	if err != nil {
 		os.Exit(1)
 	}
 
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	conn, err = net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
 		os.Exit(1)
 	}
 
-	// Send set query
-	_, err = conn.Write([]byte("set foo 10000 1"))
-	if err != nil {
-		os.Exit(1)
-	}
+	ch := make(chan []byte)
+	// Start a goroutine to read from our net connection
+	go func(ch chan []byte) {
+		for {
+			// try to read the data
+			data := make([]byte, 512)
+			_, err := conn.Read(data)
+			if err != nil {
+				// send an error if it's encountered
+				os.Exit(1)
+			}
+			// send data if we read some.
+			ch <- data
+		}
+	}(ch)
+
+	//#1 TestCase SET
+	serverCmd("set foo 10000 1\n")
+	time.Sleep(time.Duration(1) * time.Second)
+	serverCmd("bar")
+	serverReply := make([]byte, 512)
+	serverReply = <-ch
+	assert("OK", strings.Fields(string(serverReply))[0], t)
 
 	time.Sleep(time.Duration(1) * time.Second)
 
-	// Send the value to be set, "bar" in this case
-	_, err = conn.Write([]byte("bar"))
-	if err != nil {
-		os.Exit(1)
-	}
-
-	reply := make([]byte, 1024)
-
-	_, err = conn.Read(reply)
-	if err != nil {
-		os.Exit(1)
-	}
-
-	// Assert that the Server responds with "OK"/ Setting works
-	if strings.Fields(string(reply))[0] != "OK" {
-		t.Error("Setting did not work as expected.")
-	}
-
+	//#2 TestCase GET
+	serverCmd("get foo\n")
+	serverReply = make([]byte, 512)
+	serverReply = <-ch
+	assert("bar", strings.Fields(string(serverReply))[2], t)
 	time.Sleep(time.Duration(1) * time.Second)
-
-	// Send get query, "foo" as the key
-	_, err = conn.Write([]byte("get foo\r\n"))
-	if err != nil {
-		os.Exit(1)
-	}
-
-	time.Sleep(time.Duration(1) * time.Second)
-
-	reply = make([]byte, 1024)
-
-	_, err = conn.Read(reply)
-	if err != nil {
-		os.Exit(1)
-	}
-	// Extract the value from the response given by the Server
-	fLine := strings.Split(string(reply), "\n")
-	val := strings.Fields(string(fLine[2]))[0]
-	actual := "bar"
-
-	strSlice1 := fmt.Sprintf("%T", actual)
-	strSlice2 := fmt.Sprintf("%T", val)
-
-	// Assert that get value are same/ Getting works
-	if strSlice2 != strSlice1 {
-		t.Error("Getting did not work as expected.")
-	}
 
 	conn.Close()
+
+}
+
+func serverCmd(cmd string) {
+
+	_, err := conn.Write([]byte(cmd))
+	if err != nil {
+		os.Exit(1)
+	}
+
+}
+func assert(expectedResponse string, serverResponse string, t *testing.T) {
+
+	fmt.Println(len(serverResponse))
+	fmt.Println(len(expectedResponse))
+
+	equal, _ := regexp.MatchString(expectedResponse, serverResponse)
+	if !equal {
+		t.Error("Expected " + expectedResponse + " , Got " + serverResponse + " instead.")
+	}
+
 }
